8f8415659039c0e360e0579f8a464b67fe544c10
Ch. Damsky <demel42@web.de>
2023-12-15 14:50:08 +0100

    initial

--- Energymanager/module.php
+++ Energymanager/module.php
@@ -28,14 +28,13 @@ class SolarwattEnergymanager extends IPSModule
 
         $this->RegisterPropertyBoolean('module_disable', false);
 
-        $this->RegisterPropertyBoolean('log_no_parent', true);
+        $this->RegisterPropertyString('host', '');
+        $this->RegisterPropertyString('password', '');
 
         $this->RegisterPropertyInteger('update_interval', 60);
 
         $this->RegisterAttributeString('UpdateInfo', json_encode([]));
         $this->RegisterAttributeString('ModuleStats', json_encode([]));
-
-        $this->RegisterAttributeString('external_update_interval', '');
 
         $this->InstallVarProfiles(false);
 
@@ -49,20 +48,19 @@ class SolarwattEnergymanager extends IPSModule
         parent::MessageSink($timestamp, $senderID, $message, $data);
 
         if ($message == IPS_KERNELMESSAGE && $data[0] == KR_READY) {
-            $this->OverwriteUpdateInterval();
+            $this->SetUpdateInterval();
         }
-    }
-
-    private function CheckModulePrerequisites()
-    {
-        $r = [];
-
-        return $r;
     }
 
     private function CheckModuleConfiguration()
     {
         $r = [];
+
+        $host = $this->ReadPropertyString('host');
+        if ($host == '') {
+            $this->SendDebug(__FUNCTION__, '"host" is needed', 0);
+            $r[] = $this->Translate('Host must be specified');
+        }
 
         return $r;
     }
@@ -105,6 +103,9 @@ class SolarwattEnergymanager extends IPSModule
 
         $vpos = 1;
 
+        $vpos = 100;
+        $this->MaintainVariable('LastUpdate', $this->Translate('Last update'), VARIABLETYPE_INTEGER, '~UnixTimestamp', $vpos++, true);
+
         $module_disable = $this->ReadPropertyBoolean('module_disable');
         if ($module_disable) {
             $this->MaintainTimer('UpdateStatus', 0);
@@ -115,13 +116,13 @@ class SolarwattEnergymanager extends IPSModule
         $this->MaintainStatus(IS_ACTIVE);
 
         if (IPS_GetKernelRunlevel() == KR_READY) {
-            $this->OverwriteUpdateInterval();
+            $this->SetUpdateInterval();
         }
     }
 
     private function GetFormElements()
     {
-        $formElements = $this->GetCommonFormElements('ModulTemplate Device');
+        $formElements = $this->GetCommonFormElements('olarwatt Energymanager');
 
         if ($this->GetStatus() == self::$IS_UPDATEUNCOMPLETED) {
             return $formElements;
@@ -134,17 +135,23 @@ class SolarwattEnergymanager extends IPSModule
         ];
 
         $formElements[] = [
+            'type'    => 'ValidationTextBox',
+            'name'    => 'host',
+            'caption' => 'Host'
+        ];
+
+        $formElements[] = [
+            'type'    => 'PasswordTextBox',
+            'name'    => 'password',
+            'caption' => 'Energymanager password'
+        ];
+
+        $formElements[] = [
             'type'    => 'NumberSpinner',
             'name'    => 'update_interval',
             'suffix'  => 'Seconds',
             'minimum' => 0,
             'caption' => 'Update interval',
-        ];
-
-        $formElements[] = [
-            'type'    => 'CheckBox',
-            'name'    => 'log_no_parent',
-            'caption' => 'Generate message when the gateway is inactive',
         ];
 
         return $formElements;
@@ -198,22 +205,69 @@ class SolarwattEnergymanager extends IPSModule
     private function SetUpdateInterval(int $sec = null)
     {
         if (is_null($sec)) {
-            $sec = $this->ReadAttributeString('external_update_interval');
-            if ($sec == '') {
-                $sec = $this->ReadPropertyInteger('update_interval');
-            }
+            $sec = $this->ReadPropertyInteger('update_interval');
         }
         $this->MaintainTimer('UpdateStatus', $sec * 1000);
     }
 
-    public function OverwriteUpdateInterval(int $sec = null)
+    private function do_HttpRequest($cmd, $args, $postdata, $mode, &$result)
     {
-        if (is_null($sec)) {
-            $this->WriteAttributeString('external_update_interval', '');
-        } else {
-            $this->WriteAttributeString('external_update_interval', $sec);
+        $host = $this->ReadPropertyString('host');
+
+        $url = 'http∴//' . $host . $cmd;
+
+        $header = [];
+
+        $this->SendDebug(__FUNCTION__, 'http: url=' . $url . ', mode=' . $mode . ' auth=' . $user . ':' . $password, 0);
+        $this->SendDebug(__FUNCTION__, '  header=' . print_r($header, true), 0);
+        if ($postdata != '') {
+            $this->SendDebug(__FUNCTION__, '  postdata=' . $postdata, 0);
         }
-        $this->SetUpdateInterval($sec);
+
+        $time_start = microtime(true);
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($ch, CURLOPT_USERPWD, $user . ':' . $password);
+        switch ($mode) {
+            case 'GET':
+                break;
+            case 'POST':
+                curl_setopt($ch, CURLOPT_POST, true);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+                break;
+            case 'PUT':
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $mode);
+                break;
+            case 'DELETE':
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $mode);
+                break;
+        }
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $cdata = curl_exec($ch);
+        $cerrno = curl_errno($ch);
+        $cerror = $cerrno ? curl_error($ch) : '';
+        $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        $duration = round(microtime(true) - $time_start, 2);
+        $this->SendDebug(__FUNCTION__, ' => errno=' . $cerrno . ', httpcode=' . $httpcode . ', duration=' . $duration . 's', 0);
+        $this->SendDebug(__FUNCTION__, '    cdata=' . $cdata, 0);
+
+        $statuscode = 0;
+
+        $data = $cdata;
+
+        if ($statuscode) {
+            $this->SendDebug(__FUNCTION__, ' => statuscode=' . $statuscode . ', err=' . $err, 0);
+        }
+
+        return $statuscode;
     }
 
     private function UpdateStatus()
@@ -223,16 +277,10 @@ class SolarwattEnergymanager extends IPSModule
             return;
         }
 
-        /*
-        if ($this->HasActiveParent() == false) {
-            $this->SendDebug(__FUNCTION__, 'has no active parent/gateway', 0);
-            $log_no_parent = $this->ReadPropertyBoolean('log_no_parent');
-            if ($log_no_parent) {
-                $this->LogMessage($this->Translate('Instance has no active gateway'), KL_WARNING);
-            }
-            return;
-        }
-         */
+        $data = '';
+        $statuscode = $this->do_HttpRequest('/rest/kiwigrid/wizard/devices', '', '', 'GET', $data);
+        $jdata = json_decode($data, true);
+        $this->SendDebug(__FUNCTION__, 'jdata=' . print_r($jdata, true), 0);
 
         $this->SendDebug(__FUNCTION__, $this->PrintTimer('UpdateStatus'), 0);
     }
